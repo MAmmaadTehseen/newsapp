26eea2fa182794a0e6c11a026f1c2d739ad0df38
Muhammad Ammaad Tehseen <ammadtehseenkhan@gmail.com>
2023-12-30 21:10:24 +0500

    30-12-23

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,22 +2,20 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
+    let { title, description, imageUrl, newsUrl } = this.props;
     return (
-      <div>
+      <div className="m-4">
         <div className="card" style={{ width: "18rem" }}>
-          <img
-            src="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"
-            className="card-img-top"
-            alt="..."
-          />
+          <img src={imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="/" className="btn btn-primary">
-              Go somewhere
+            <h5 className="card-title">{title}....</h5>
+            <p className="card-text">{description}....</p>
+            <a
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-primary btn-sm"
+            >
+              Read More
             </a>
           </div>
         </div>
