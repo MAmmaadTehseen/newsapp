4a7f969eca2aa4483bb3b21a059df2bec4eb2768
Muhammad Ammaad Tehseen <ammadtehseenkhan@gmail.com>
2024-02-03 15:22:38 +0500

    Update NewsItem.js
    class based to function based

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,45 +1,45 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class NewsItem extends Component {
-  render() {
-    let { title, description, imageUrl, newsUrl, author, publishedAt, source } =
-      this.props;
-    return (
-      <div className="m-4">
-        <div className="card">
-          <img
-            src={
-              !imageUrl ? "https://images.wsj.net/im-901282/social" : imageUrl
-            }
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">
-              {description ? description : "Description not avaliable"}....
-            </p>
-            <p className="card-text">
-              <small className="text-success">
-                Published by {author ? author : "unknown"} at {publishedAt}
-              </small>
-            </p>
-            <p className="card-text m-2">
-              <small className="text-warning  ">source:"{source}"</small>
-            </p>
-            <a
-              href={newsUrl}
-              target="_blank"
-              rel="noreferrer"
-              className="btn btn-primary btn-sm"
-            >
-              Read More
-            </a>
-          </div>
+const NewsItem = (props) => {
+
+  let { title, description, imageUrl, newsUrl, author, publishedAt, source } =
+    props;
+  return (
+    <div className="m-4">
+      <div className="card">
+        <img
+          src={
+            !imageUrl ? "https://images.wsj.net/im-901282/social" : imageUrl
+          }
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">
+            {description ? description : "Description not avaliable"}....
+          </p>
+          <p className="card-text">
+            <small className="text-success">
+              Published by {author ? author : "unknown"} at {publishedAt}
+            </small>
+          </p>
+          <p className="card-text m-2">
+            <small className="text-warning  ">source:"{source}"</small>
+          </p>
+          <a
+            href={newsUrl}
+            target="_blank"
+            rel="noreferrer"
+            className="btn btn-primary btn-sm"
+          >
+            Read More
+          </a>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+
 }
 
 export default NewsItem;
